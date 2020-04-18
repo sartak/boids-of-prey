74c28bcc660b7eaf6e23768631cf0964c3068fd2
Shawn M Moore <code@sartak.org>
2020-04-18 02:18:22 -0400

    hour 5

--- src/play-scene.js
+++ src/play-scene.js
@@ -136,7 +136,7 @@ export default class PlayScene extends SuperScene {
     }
 
     const player = this.physics.add.sprite(x, y, 'spritePlayer');
-    player.setMass(prop('player.mass'));
+    player.body.setMass(prop('player.mass'));
     player.setBounce(prop('player.bounce'));
     player.setDrag(prop('player.drag'));
     player.setMaxVelocity(prop('player.maxVelocity'));
@@ -170,7 +170,7 @@ export default class PlayScene extends SuperScene {
 
       const follower = followerGroup.create(xCoord + halfWidth, yCoord + halfHeight, 'spriteFollower');
 
-      follower.setMass(mass);
+      follower.body.setMass(mass);
       follower.setBounce(bounce);
       follower.setDrag(drag);
       follower.setDamping(true);
@@ -178,6 +178,7 @@ export default class PlayScene extends SuperScene {
       follower.setMaxVelocity(maxVelocity);
       follower.setCollideWorldBounds(true);
       follower.setCircle(radius);
+      follower.isFollower = true;
 
       return follower;
     });
@@ -319,6 +320,86 @@ export default class PlayScene extends SuperScene {
 
   fixedUpdate(time, dt) {
     this.processInput();
+    this.flockFollowers();
+  }
+
+  followersNearPoint(x, y, r) {
+    // overlapCirc was added in a newer version of phaser
+    return this.physics.overlapRect(x - r / 2, y - r / 2, r, r, true, false).map((body) => body.gameObject).filter((object) => object.isFollower);
+  }
+
+  flockFollowers() {
+    const {level, physics} = this;
+    const {followers, player} = level;
+
+    const px = player.x;
+    const py = player.y;
+
+    const acceleration = prop('follower.flockAcceleration');
+    const cohereRadius = prop('follower.cohereRadius');
+    const spreadRadius = prop('follower.spreadRadius');
+
+    followers.forEach((follower) => {
+      const fx = follower.x;
+      const fy = follower.y;
+
+      let tx;
+      let ty;
+
+      const cohereFollowers = this.followersNearPoint(fx, fy, cohereRadius);
+
+      const playerDistance = Math.sqrt((px - fx) ** 2 + (py - fy) ** 2);
+      if (playerDistance < cohereRadius) {
+        tx = px;
+        ty = py;
+      } else {
+        // overlapCirc was added in a newer version of phaser
+        let cx = 0;
+        let cy = 0;
+        cohereFollowers.forEach((f) => {
+          cx += f.x;
+          cy += f.y;
+        });
+        cx /= cohereFollowers.length;
+        cy /= cohereFollowers.length;
+        tx = cx;
+        ty = cy;
+      }
+
+      let spreadX = 0;
+      let spreadY = 0;
+      let spreadCount = 0;
+
+      const spreadFollowers = cohereRadius > spreadRadius ? cohereFollowers : this.followersNearPoint(fx, fy, spreadRadius);
+      spreadFollowers.forEach((f) => {
+        if (f === follower) {
+          return;
+        }
+
+        const dx = fx - f.x;
+        const dy = fy - f.y;
+        const fDistance = Math.sqrt(dx ** 2 + dy ** 2);
+        if (fDistance > spreadRadius) {
+          return;
+        }
+
+        spreadX += dx / fDistance;
+        spreadY += dy / fDistance;
+        spreadCount += 1;
+      });
+
+      const targetTheta = Math.atan2(ty - fy, tx - fx);
+      let theta = targetTheta;
+
+      if (spreadCount) {
+        spreadX /= spreadCount;
+        spreadY /= spreadCount;
+
+        theta = targetTheta * 0.5 + Math.atan2(spreadY, spreadX) * 0.5;
+      }
+
+      follower.body.setAcceleration(acceleration * Math.cos(theta), acceleration * Math.sin(theta));
+    });
   }
 
   willTransitionTo(newScene, transition) {
@@ -378,6 +459,6 @@ export default class PlayScene extends SuperScene {
   }
 
   _hot() {
-    this._hotReloadCurrentLevel();
+    // this._hotReloadCurrentLevel();
   }
 }
